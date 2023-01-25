b9e08c944c0c6c534bdc3c06cac528cbf8fe387f
august <august@DESKTOP-540BKCA>
2023-01-25 11:32:14 +0100

    Fixat removebid.cs funktion

---
+++ Repository/RemoveBid/RemoveBid.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System;
+
+namespace Auctions.WebApi.Repository.RemoveBid
+{
+    public class RemoveBidRepo
+    {
+        private readonly string connectionString;
+        public RemoveBidRepo(string _connectionString)
+        {
+            connectionString = _connectionString;
+        }
+        public void RemoveBid(int auctionId, int bidId, Guid userId, int bidPrice, DateTime bidDate, bool status)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("RemoveBid", (SqlConnection)db))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@AuctionID", auctionId);
+                    command.Parameters.AddWithValue("@BidId", bidId);
+                    command.Parameters.AddWithValue("@UserID", userId);
+                    command.Parameters.AddWithValue("@BidPrice", bidPrice);
+                    command.Parameters.AddWithValue("@BidDate", bidDate);
+                    command.Parameters.AddWithValue("@Status", status);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
