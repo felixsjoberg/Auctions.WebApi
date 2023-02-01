aa7c881d7e383fcdd8b40041ff18a7c84c9a1359
b4ngcsgo <72876160+b4ngcsgo@users.noreply.github.com>
2023-02-01 13:11:20 +0100

    Update UpdateAuction.sql

--- StoredProcedures/UpdateAuction.sql
+++ StoredProcedures/UpdateAuction.sql
@@ -1,11 +1,8 @@
 CREATE PROCEDURE [dbo].[UpdateAuction](@AuctionID INT,
-    @UserID UNIQUEIDENTIFIER,
-	@Title NVARCHAR(50),
-	@Description NVARCHAR(50),
-	@Price INT,
-	--@StartDate DATETIME2(7),
-	@EndDate DATETIME2(7),
-	@Status BIT)
+	                                   @Title NVARCHAR(50),
+	                                   @Description NVARCHAR(50),
+	                                   @Price INT,
+	                                   @EndDate DATETIME2(7))
 AS
 BEGIN
     UPDATE dbo.Auctions
@@ -13,24 +10,7 @@ BEGIN
     Title = @Title,
     Description = @Description,
     EndDate = @EndDate,
-    Status = @Status
-    where AuctionID = @AuctionID
-    SELECT	Auctions.AuctionID, Bids.AuctionID FROM Auctions INNER JOIN Bids 
-	ON Auctions.AuctionID = Bids.AuctionID
-
-    UPDATE dbo.Auctions
-    set
-    Title = @Title,
-    Description = @Description,
-    EndDate = @EndDate,
-    Status = @Status,
     Price = @Price
     where AuctionID = @AuctionID
-
-
-
-    SELECT *
-    FROM Auctions
-    WHERE AuctionID = @AuctionID
 END
 GO
