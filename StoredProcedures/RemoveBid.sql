0814b23ba36c8558369d8f64cda734350f39dc79
Felix Sjöberg <felixsjoberg95@gmail.com>
2023-02-02 11:21:21 +0100

    update sP removebid

--- StoredProcedures/RemoveBid.sql
+++ StoredProcedures/RemoveBid.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE [dbo].[RemoveBid](
+ALTER PROCEDURE [dbo].[RemoveBid](
 	@BidId INT)
 	
 AS
@@ -6,13 +6,20 @@ BEGIN
 	DECLARE @auctionCurrentBalance MONEY;
     DECLARE @bidPrice MONEY;
     DECLARE @getAuctionID INT;
+	DECLARE @BidCount INT;
 
     SELECT @auctionCurrentBalance = Price FROM Auctions INNER JOIN Bids ON Bids.AuctionID = Auctions.AuctionID WHERE bids.BidID = @BidId;
     SELECT @getAuctionID = Auctions.AuctionID FROM Auctions INNER JOIN Bids ON Bids.AuctionID = Auctions.AuctionID WHERE bids.BidID = @BidId;
     SELECT @bidPrice = BidPrice FROM Bids WHERE BidId = @BidId;
-
-
+	SELECT @BidCount = COUNT(*) FROM BIDS WHERE BidId = @BidId;
+	
+	IF @BidCount = 0
+    RETURN;
+			
     UPDATE Auctions SET Price = @auctionCurrentBalance - @bidPrice WHERE AuctionID = @getAuctionID;
+	
+	DELETE FROM Bids
+    WHERE  BidID=@BidId
 
 END
 GO
